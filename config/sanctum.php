531a5c2f8a83ce1828a18722e0031c631a4dadb5
Zdravko Koev <zdravko.j.koev@gmail.com>
2024-02-02 16:32:14 +0000

    Configure user authorisation methods and routes

--- config/sanctum.php
+++ config/sanctum.php
@@ -78,6 +78,10 @@ return [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
         'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+        'stateful' => [
+            'web',
+            'stateful:session',
+        ],
     ],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // Add other routes that require authentication here
 });
 
 
-Route::get('/sigup', [AuthController::class, 'signup']);
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
