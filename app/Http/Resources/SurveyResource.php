917b364ef14175dc7d020b06a589b2b136b93d8b
Zdravko Koev <zdravko.j.koev@gmail.com>
2024-02-08 15:45:31 +0000

    Loading surveys correctly

--- app/Http/Resources/SurveyResource.php
+++ app/Http/Resources/SurveyResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 
 class SurveyResource extends JsonResource
 {
@@ -17,7 +18,14 @@ class SurveyResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'slug' => $this->slug
+            'slug' => $this->slug,
+            'image_url' => $this->image ? URL::to($this->image) : null,
+            'status' => !!$this->status,
+            'description' => $this->description,
+            'created_at' => $this->created_at instanceof \DateTime ? $this->created_at->format('Y-m-d H:m:s') : $this->created_at,
+            'updated_at' => $this->updated_at instanceof \DateTime ? $this->updated_at->format('Y-m-d H:m:s') : $this->updated_at,
+            'expire_date' => $this->expire_date instanceof \DateTime ? $this->expire_date->format('Y-m-d H:m:s') : $this->expire_date,
+            'questions' => SurveyQuestionResource::collection($this->questions)
         ];
     }
 }
