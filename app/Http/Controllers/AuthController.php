378c39575f0b71948868988f736a4e5adcc065af
Zdravko Koev <zdravko.j.koev@gmail.com>
2024-02-08 12:20:48 +0000

    Add route for getting user data

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,69 +1,5 @@
 <?php
 
-// namespace App\Http\Controllers;
-
-// use App\Http\Requests\LoginRequest;
-// use App\Http\Requests\SignupRequest;
-// use App\Models\User;
-// use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Auth;
-
-// class AuthController extends Controller
-// {
-//     public function signup(SignupRequest $request)
-//     {
-//         $data = $request->validated();
-
-//         $user = User::create([
-//             'name' => $data['name'],
-//             'email' => $data['email'],
-//             'password' => bcrypt($data['password'])
-//         ]);
-
-//         $token = $user->createToken('main')->plainTextToken;
-
-//         return response([
-//             'user' => $user,
-//             'token' => $token
-//         ]);
-//     }
-
-//     public function login(LoginRequest $request)
-//     {
-//         $credentials = $request->validated();
-//         $remember = $credentials['remember'] ?? false;
-//         unset($credentials['remember']);
-
-//         if (!Auth::attempt($credentials, $remember)) {
-//             return response([
-//                 'error' => 'The Provided credentials are not correct'
-//             ], 422);
-//         }
-
-//         $user = Auth::user();
-//         $token = $user->createToken('main')->plainTextToken;
-
-//         return response([
-//             'user' => $user,
-//             'token' => $token
-//         ]);
-//     }
-
-//     public function logout(Request $request)
-//     {
-//         $user = Auth::user();
-//         $user->currentAccessToken()->delete();
-
-//         return response([
-//             'success' => true
-//         ]);
-//     }
-
-//     public function me(Request $request)
-//     {
-//         return $request->user();
-//     }
-// }
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
@@ -78,7 +14,6 @@ class AuthController extends Controller
     {
         $data = $request->validated();
 
-        /** @var \App\Models\User $user */
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
@@ -104,6 +39,7 @@ class AuthController extends Controller
             ], 422);
         }
         $user = Auth::user();
+        /** @var User $user */
         $token = $user->createToken('main')->plainTextToken;
 
         return response([
@@ -116,7 +52,6 @@ class AuthController extends Controller
     {
         /** @var User $user */
         $user = Auth::user();
-        // Revoke the token that was used to authenticate the current request...
         $user->currentAccessToken()->delete();
 
         return response([
